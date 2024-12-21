5c13c92ad8f43b028d0dee53654736b929d710a0
Stoupy51 <stoupy51@gmail.com>
2024-12-21 01:48:14 +0100

    fix: 🐛 Fixed `github_release.py`

--- github_release.py
+++ github_release.py
@@ -8,7 +8,7 @@ import os
 
 # Constants
 ROOT: str = clean_path(os.path.dirname(os.path.abspath(__file__)))
-CREDENTIALS_PATH: str = ROOT + "/credentials.yml"
+CREDENTIALS_PATH: str = "~/stouputils/credentials.yml"
 TEMPORARY_FOLDER: str = f"{ROOT}/__temporary__"
 GITHUB_CONFIG: dict[str, Any] = {
 	"project_name": "stouputils",
@@ -20,14 +20,15 @@ GITHUB_CONFIG: dict[str, Any] = {
 credentials: dict[str, Any] = load_credentials(CREDENTIALS_PATH)
 
 # Get the latest build in a temporary folder
-os.makedirs(TEMPORARY_FOLDER, exist_ok=True)
-super_copy(f"{ROOT}/dist/stouputils-{current_version}.tar.gz", TEMPORARY_FOLDER)
-super_copy(f"{ROOT}/dist/stouputils-{current_version}.py3-none-any.whl", TEMPORARY_FOLDER)
+try:
+	os.makedirs(TEMPORARY_FOLDER, exist_ok=True)
+	super_copy(f"{ROOT}/dist/stouputils-{current_version}.tar.gz", TEMPORARY_FOLDER)
+	super_copy(f"{ROOT}/dist/stouputils-{current_version}-py3-none-any.whl", TEMPORARY_FOLDER)
 
-# Upload to GitHub
-changelog: str = upload_to_github(credentials, GITHUB_CONFIG)
-info(f"\n{changelog}")
+	# Upload to GitHub
+	changelog: str = upload_to_github(credentials, GITHUB_CONFIG)
 
-# Delete the temporary folder
-shutil.rmtree(TEMPORARY_FOLDER)
+finally:
+	# Delete the temporary folder
+	shutil.rmtree(TEMPORARY_FOLDER)
 
