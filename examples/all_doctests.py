0bd4d87bafb1d6e4f18712e137773641813856ea
Stoupy51 <stoupy51@gmail.com>
2025-03-06 12:02:25 +0100

    fix: 🐛 Fixed all_doctests and delta_backup examples

--- examples/all_doctests.py
+++ examples/all_doctests.py
@@ -5,7 +5,7 @@ import stouputils as stp
 # Main
 @stp.measure_time(stp.info, message="All doctests finished")
 def main() -> None:
-	FOLDER_TO_TEST: str = stp.get_root_path(__file__, 1) + "/src"
+	FOLDER_TO_TEST: str = stp.get_root_path(__file__, 1)
 	stp.launch_tests(FOLDER_TO_TEST)
 
 if __name__ == "__main__":
--- examples/delta_backup.py
+++ examples/delta_backup.py
@@ -2,6 +2,7 @@
 # Example of use:
 # py examples/delta_backup.py delta "src" "backup" -x "*pycache*"
 
-import stouputils as stp
-stp.backup_cli()
+if __name__ == "__main__":
+	import stouputils as stp
+	stp.backup_cli()
 
