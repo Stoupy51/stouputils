5629697d20897b2425a20ae098763cea31bac5a7
Stoupy51 <stoupy51@gmail.com>
2025-03-17 17:02:05 +0100

    chore: 🔧 Swapped color in debug and alt_debug functions in the print module

--- stouputils/print.py
+++ stouputils/print.py
@@ -83,19 +83,19 @@ def info(*values: Any, color: str = GREEN, text: str = "INFO ", prefix: str = ""
 				print(remove_colors(message), *(remove_colors(str(v)) for v in values), file=log_file, **print_kwargs)
 
 def debug(*values: Any, **print_kwargs: Any) -> None:
-	""" Print a debug message looking like "[DEBUG HH:MM:SS] message" in blue by default. """
-	if "text" not in print_kwargs:
-		print_kwargs["text"] = "DEBUG"
-	if "color" not in print_kwargs:
-		print_kwargs["color"] = BLUE
-	info(*values, **print_kwargs)
-
-def alt_debug(*values: Any, **print_kwargs: Any) -> None:
 	""" Print a debug message looking like "[DEBUG HH:MM:SS] message" in cyan by default. """
 	if "text" not in print_kwargs:
 		print_kwargs["text"] = "DEBUG"
 	if "color" not in print_kwargs:
 		print_kwargs["color"] = CYAN
+	info(*values, **print_kwargs)
+
+def alt_debug(*values: Any, **print_kwargs: Any) -> None:
+	""" Print a debug message looking like "[DEBUG HH:MM:SS] message" in blue by default. """
+	if "text" not in print_kwargs:
+		print_kwargs["text"] = "DEBUG"
+	if "color" not in print_kwargs:
+		print_kwargs["color"] = BLUE
 	info(*values, **print_kwargs)
 
 def suggestion(*values: Any, **print_kwargs: Any) -> None:
