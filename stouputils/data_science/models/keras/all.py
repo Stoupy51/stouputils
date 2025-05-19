39f578ec7b36c0ca2781b283ba551d09d7101f6c
Stoupy51 <stoupy51@gmail.com>
2025-05-19 11:33:54 +0200

    feat: 🔥 Removed ResNetRS models
    - Eliminated unused ResNetRS models from the imports in all.py and keras/all.py.
    - Updated CLASS_MAP in all.py to reflect the removal of ResNetRS models.
    - Cleaned up the resnet.py file by removing ResNetRS model classes and their associated methods.

--- stouputils/data_science/models/keras/all.py
+++ stouputils/data_science/models/keras/all.py
@@ -4,18 +4,7 @@ from .convnext import ConvNeXtBase, ConvNeXtLarge, ConvNeXtSmall, ConvNeXtTiny, 
 from .densenet import DenseNet121, DenseNet169, DenseNet201
 from .efficientnet import EfficientNetB0, EfficientNetV2B0, EfficientNetV2L, EfficientNetV2M, EfficientNetV2S
 from .mobilenet import MobileNet, MobileNetV2, MobileNetV3Large, MobileNetV3Small
-from .resnet import (
-    ResNet50V2,
-    ResNet101V2,
-    ResNet152V2,
-    ResNetRS50,
-    ResNetRS101,
-    ResNetRS152,
-    ResNetRS200,
-    ResNetRS270,
-    ResNetRS350,
-    ResNetRS420,
-)
+from .resnet import ResNet50V2, ResNet101V2, ResNet152V2
 from .squeezenet import SqueezeNet
 from .vgg import VGG16, VGG19
 from .xception import Xception
@@ -43,13 +32,6 @@ __all__ = [
     "ResNet50V2",
     "ResNet101V2",
     "ResNet152V2",
-    "ResNetRS50",
-    "ResNetRS101",
-    "ResNetRS152",
-    "ResNetRS200",
-    "ResNetRS270",
-    "ResNetRS350",
-    "ResNetRS420",
     "SqueezeNet",
     "Xception",
 ]
--- stouputils/data_science/models/keras/resnet.py
+++ stouputils/data_science/models/keras/resnet.py
@@ -11,14 +11,6 @@ Available models:
     - ResNet50V2
     - ResNet101V2
     - ResNet152V2
-- ResNetRS family: ResNet models with revisited scaling for better efficiency
-    - ResNetRS50
-    - ResNetRS101
-    - ResNetRS152
-    - ResNetRS200
-    - ResNetRS270
-    - ResNetRS350
-    - ResNetRS420
 
 All models support transfer learning from ImageNet pre-trained weights.
 """
@@ -29,13 +21,6 @@ All models support transfer learning from ImageNet pre-trained weights.
 from __future__ import annotations
 
 from keras.models import Model
-from keras.src.applications.resnet_rs import ResNetRS50 as ResNetRS50_keras
-from keras.src.applications.resnet_rs import ResNetRS101 as ResNetRS101_keras
-from keras.src.applications.resnet_rs import ResNetRS152 as ResNetRS152_keras
-from keras.src.applications.resnet_rs import ResNetRS200 as ResNetRS200_keras
-from keras.src.applications.resnet_rs import ResNetRS270 as ResNetRS270_keras
-from keras.src.applications.resnet_rs import ResNetRS350 as ResNetRS350_keras
-from keras.src.applications.resnet_rs import ResNetRS420 as ResNetRS420_keras
 from keras.src.applications.resnet_v2 import ResNet50V2 as ResNet50V2_keras
 from keras.src.applications.resnet_v2 import ResNet101V2 as ResNet101V2_keras
 from keras.src.applications.resnet_v2 import ResNet152V2 as ResNet152V2_keras
@@ -58,41 +43,9 @@ class ResNet152V2(BaseKeras):
 	def _get_base_model(self) -> Model:
 		return ResNet152V2_keras(include_top=False, classes=self.num_classes)
 
-class ResNetRS50(BaseKeras):
-	def _get_base_model(self) -> Model:
-		return ResNetRS50_keras(include_top=False, classes=self.num_classes)
-
-class ResNetRS101(BaseKeras):
-	def _get_base_model(self) -> Model:
-		return ResNetRS101_keras(include_top=False, classes=self.num_classes)
-
-class ResNetRS152(BaseKeras):
-	def _get_base_model(self) -> Model:
-		return ResNetRS152_keras(include_top=False, classes=self.num_classes)
-
-class ResNetRS200(BaseKeras):
-	def _get_base_model(self) -> Model:
-		return ResNetRS200_keras(include_top=False, classes=self.num_classes)
-
-class ResNetRS270(BaseKeras):
-	def _get_base_model(self) -> Model:
-		return ResNetRS270_keras(include_top=False, classes=self.num_classes)
-
-class ResNetRS350(BaseKeras):
-	def _get_base_model(self) -> Model:
-		return ResNetRS350_keras(include_top=False, classes=self.num_classes)
-
-class ResNetRS420(BaseKeras):
-	def _get_base_model(self) -> Model:
-		return ResNetRS420_keras(include_top=False, classes=self.num_classes)
-
 
 # Docstrings
-for model in [
-	ResNet50V2, ResNet101V2, ResNet152V2,
-	ResNetRS50, ResNetRS101, ResNetRS152, ResNetRS200,
-	ResNetRS270, ResNetRS350, ResNetRS420
-]:
+for model in [ResNet50V2, ResNet101V2, ResNet152V2]:
 	model.__doc__ = MODEL_DOCSTRING.format(model=model.__name__)
 	model.class_routine = simple_cache(model.class_routine)
 	model.class_routine.__doc__ = CLASS_ROUTINE_DOCSTRING.format(model=model.__name__)
