6581d20f3fbfe00bbae63164b98864a6a3cebd06
Stoupy51 <stoupy51@gmail.com>
2025-05-19 11:27:30 +0200

    chore: 🔧 Update imports and path handling in all_doctests.py
    - Ensured local stouputils package is prioritized over installed versions by modifying sys.path.
    - Simplified function calls by directly importing necessary functions from stouputils.
    - Enhanced error handling during module imports in launch_tests function to provide warnings for failed imports.

--- examples/all_doctests.py
+++ examples/all_doctests.py
@@ -1,15 +1,20 @@
 
 # Imports
 import sys
+import os
 
-import stouputils as stp
+# Ensure the local stouputils package is used instead of any installed version in site-packages
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+# Import stouputils
+from stouputils import measure_time, info, get_root_path, launch_tests
 
 
 # Main
-@stp.measure_time(stp.info, message="All doctests finished")
+@measure_time(info, message="All doctests finished")
 def main() -> None:
-	FOLDER_TO_TEST: str = stp.get_root_path(__file__, 1)
-	if stp.launch_tests(f"{FOLDER_TO_TEST}/stouputils") > 0:
+	FOLDER_TO_TEST: str = get_root_path(__file__, 1)
+	if launch_tests(f"{FOLDER_TO_TEST}/stouputils") > 0:
 		sys.exit(1)
 
 if __name__ == "__main__":
