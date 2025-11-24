9f927f9b413c9be0bca828bd218a8844f08c48a2
Stoupy51 <stoupy51@gmail.com>
2025-11-24 11:43:06 +0100

    feat(collections): ✨ Add sort_dict_keys function to sort dictionary keys based on a specified order

--- stouputils/collections.py
+++ stouputils/collections.py
@@ -2,6 +2,7 @@
 This module provides utilities for collection manipulation:
 
 - unique_list: Remove duplicates from a list while preserving order using object id, hash or str
+- sort_dict_keys: Sort dictionary keys using a given order list (ascending or descending)
 - array_to_disk: Easily handle large numpy arrays on disk using zarr for efficient storage and access.
 
 .. image:: https://raw.githubusercontent.com/Stoupy51/stouputils/refs/heads/main/assets/collections_module.gif
@@ -13,12 +14,14 @@ import atexit
 import os
 import shutil
 import tempfile
-from typing import Any, Literal
+from typing import Any, Literal, TypeVar
 
 import numpy as np
 import zarr  # pyright: ignore[reportMissingTypeStubs]
 from numpy.typing import NDArray
 
+# Typing
+T = TypeVar("T")
 
 # Functions
 def unique_list(list_to_clean: list[Any], method: Literal["id", "hash", "str"] = "str") -> list[Any]:
@@ -68,6 +71,28 @@ def unique_list(list_to_clean: list[Any], method: Literal["id", "hash", "str"] =
 
 	# Return the cleaned list
 	return result
+
+def sort_dict_keys(dictionary: dict[T, Any], order: list[T], reverse: bool = False) -> dict[T, Any]:
+	""" Sort dictionary keys using a given order list (reverse optional)
+
+	Args:
+		dictionary	(dict[T, Any]):	The dictionary to sort
+		order		(list[T]):		The order list
+		reverse		(bool):			Whether to sort in reverse order (given to sorted function which behaves differently than order.reverse())
+	Returns:
+		dict[T, Any]: The sorted dictionary
+
+	Examples:
+		>>> sort_dict_keys({'b': 2, 'a': 1, 'c': 3}, order=["a", "b", "c"])
+		{'a': 1, 'b': 2, 'c': 3}
+
+		>>> sort_dict_keys({'b': 2, 'a': 1, 'c': 3}, order=["a", "b", "c"], reverse=True)
+		{'c': 3, 'b': 2, 'a': 1}
+
+		>>> sort_dict_keys({'b': 2, 'a': 1, 'c': 3, 'd': 4}, order=["c", "b"])
+		{'c': 3, 'b': 2, 'a': 1, 'd': 4}
+	"""
+	return dict(sorted(dictionary.items(), key=lambda x: order.index(x[0]) if x[0] in order else len(order), reverse=reverse))
 
 def array_to_disk(
 	data: NDArray[Any] | zarr.Array,
